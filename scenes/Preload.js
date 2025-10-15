0941dae720d4c24733d3fc5d39e2c2d3136c7cc4
JoaquinVeron <veronjoaquin180@gmail.com>
2025-10-15 00:44:52 -0300

    Imagenes Resolv3

--- scenes/Preload.js
+++ scenes/Preload.js
@@ -9,58 +9,58 @@ export default class Preload extends Phaser.Scene {
     this.load.setPath("assets");
     
     // Imagenes
-    this.load.image("Calle", "public/assets/Calle.png");
-    this.load.image("Celeste1", "public/assets/1.png");
-    this.load.image("Naranja2", "public/assets/2.png");
-    this.load.image("Celeste", "public/assets/Celeste.png");
-    this.load.image("Naranja", "public/assets/Naranja.png");
-    this.load.image("Auto", "public/assets/auto.png");
-    this.load.image("Camion", "public/assets/Camion.png");
-    this.load.image("Camion2", "public/assets/Camion2.png");
-    this.load.image("Moto", "public/assets/Moto.png");
-    this.load.image("Arena", "public/assets/Arena.png");
-    this.load.image("Balde", "public/assets/Balde.png");
-    this.load.image("BaldeArena", "public/assets/BaldeArena.png");
-    this.load.image("BaldeGrava", "public/assets/BaldeGrava.png");
-    this.load.image("BaldeCemento", "public/assets/BaldeCemento.png");
-    this.load.image("Barrera", "public/assets/BloqueDivisor.png");
-    this.load.image("Borde", "public/assets/Borde.png");
-    this.load.image("Carretilla", "public/assets/Carretilla.png");
-    this.load.image("Cemento", "public/assets/Cemento.png");
-    this.load.image("Coca", "public/assets/Coca.png");
-    this.load.image("Construccion", "public/assets/Construccion.png");
-    this.load.image("Construccion2", "public/assets/Construccion2.png");
-    this.load.image("Construccion3", "public/assets/Construccion3.png");
-    this.load.image("Cuchara", "public/assets/Cuchara.png");
-    this.load.image("Espatula", "public/assets/Espatula.png");
-    this.load.image("Grava", "public/assets/Grava.png");
-    this.load.image("Ladrillos", "public/assets/Ladrillos.png");
-    this.load.image("Ladrillo", "public/assets/Ladrillo.png");
-    this.load.image("Llana", "public/assets/Llana.png");
-    this.load.image("Mezcladora", "public/assets/Mezcladora.png");
-    this.load.image("MezcladoraLlena", "public/assets/MezcladoraLlena.png");
-    this.load.image("MiraCeleste", "public/assets/MiraCeleste.png");
-    this.load.image("MiraNaranja", "public/assets/MiraNaranja.png");
-    this.load.image("Pala", "public/assets/Pala.png");
-    this.load.image("Pared", "public/assets/Pared.png");
-    this.load.image("Pasto", "public/assets/Pasto.png");
-    this.load.image("Pasto2", "public/assets/Pasto2.png");
-    this.load.image("PastoDetalle", "public/assets/PastoDetalle.png");
-    this.load.image("Sombreado", "public/assets/Sombreado.png");
+    this.load.image("Calle", "Calle.png");
+    this.load.image("Celeste1", "1.png");
+    this.load.image("Naranja2", "2.png");
+    this.load.image("Celeste", "Celeste.png");
+    this.load.image("Naranja", "Naranja.png");
+    this.load.image("Auto", "auto.png");
+    this.load.image("Camion", "Camion.png");
+    this.load.image("Camion2", "Camion2.png");
+    this.load.image("Moto", "Moto.png");
+    this.load.image("Arena", "Arena.png");
+    this.load.image("Balde", "Balde.png");
+    this.load.image("BaldeArena", "BaldeArena.png");
+    this.load.image("BaldeGrava", "BaldeGrava.png");
+    this.load.image("BaldeCemento", "BaldeCemento.png");
+    this.load.image("Barrera", "BloqueDivisor.png");
+    this.load.image("Borde", "Borde.png");
+    this.load.image("Carretilla", "Carretilla.png");
+    this.load.image("Cemento", "Cemento.png");
+    this.load.image("Coca", "Coca.png");
+    this.load.image("Construccion", "Construccion.png");
+    this.load.image("Construccion2", "Construccion2.png");
+    this.load.image("Construccion3", "Construccion3.png");
+    this.load.image("Cuchara", "Cuchara.png");
+    this.load.image("Espatula", "Espatula.png");
+    this.load.image("Grava", "Grava.png");
+    this.load.image("Ladrillos", "Ladrillos.png");
+    this.load.image("Ladrillo", "Ladrillo.png");
+    this.load.image("Llana", "Llana.png");
+    this.load.image("Mezcladora", "Mezcladora.png");
+    this.load.image("MezcladoraLlena", "MezcladoraLlena.png");
+    this.load.image("MiraCeleste", "MiraCeleste.png");
+    this.load.image("MiraNaranja", "MiraNaranja.png");
+    this.load.image("Pala", "Pala.png");
+    this.load.image("Pared", "Pared.png");
+    this.load.image("Pasto", "Pasto.png");
+    this.load.image("Pasto2", "Pasto2.png");
+    this.load.image("PastoDetalle", "PastoDetalle.png");
+    this.load.image("Sombreado", "Sombreado.png");
 
     // Sonidos
-    this.load.audio("Arena", "public/assets/Arena.mp3");
-    this.load.audio("Bocina", "public/assets/Bocina.mp3");
-    this.load.audio("Bocina2", "public/assets/Bocina2.mp3");
-    this.load.audio("Bocina3", "public/assets/Bocina3.mp3");
-    this.load.audio("RecGrava", "public/assets/RecGrava.mp3");
-    this.load.audio("SonidoCarretera", "public/assets/SonidoCarretera.mp3");
-    this.load.audio("SonidoCarretera2", "public/assets/SonidoCarretera2.mp3");
-    this.load.audio("SonidoConstruccion", "public/assets/sonidoConstruccion.mp3");
-    this.load.audio("RuidoMoto", "public/assets/mentiraColgala.mp3");
+    this.load.audio("Arena", "Arena.mp3");
+    this.load.audio("Bocina", "Bocina.mp3");
+    this.load.audio("Bocina2", "Bocina2.mp3");
+    this.load.audio("Bocina3", "Bocina3.mp3");
+    this.load.audio("RecGrava", "RecGrava.mp3");
+    this.load.audio("SonidoCarretera", "SonidoCarretera.mp3");
+    this.load.audio("SonidoCarretera2", "SonidoCarretera2.mp3");
+    this.load.audio("SonidoConstruccion", "sonidoConstruccion.mp3");
+    this.load.audio("RuidoMoto", "mentiraColgala.mp3");
 
     // Videos
-    this.load.video("Intro", "public/assets/TelevisaPresenta.mp4");
+    this.load.video("Intro", "TelevisaPresenta.mp4");
   }
 
   create() {
